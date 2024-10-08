0c151829a917ec5e5a27c503ce12eeb6d29cb1a9
thanhnx12 <thanhnx1209@gmail.com>
2024-10-08 04:34:23 +0000

    fix

--- SCKD/train.sh
+++ SCKD/train.sh
@@ -1,7 +1,7 @@
-python main.py --task tacred --shot 5 --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10 
-python main.py --task tacred --shot 5 --mixup --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10 
-python main.py --task FewRel --shot 5 --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10 
-python main.py --task FewRel --shot 5 --mixup --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10
+python main.py --task tacred --shot 5 --SAM --SAM_type current --step1_epochs 15 --step2_epochs 10 --step3_epochs 10 
+python main.py --task tacred --shot 5 --mixup --SAM --SAM_type current --step1_epochs 15 --step2_epochs 10 --step3_epochs 10 
+python main.py --task FewRel --shot 5 --SAM --SAM_type current --step1_epochs 15 --step2_epochs 10 --step3_epochs 10 
+python main.py --task FewRel --shot 5 --mixup --SAM --SAM_type current --step1_epochs 15 --step2_epochs 10 --step3_epochs 10
 python main-mmi.py --task tacred --shot 5 --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10 
 python main-mmi.py --task tacred --shot 5 --mixup --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10 
 python main-mmi.py --task FewRel --shot 5 --SAM --SAM_type current --step1_epochs 5 --step2_epochs 10 --step3_epochs 10 
